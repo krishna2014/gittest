98f55c3fe880486647c39c852a53ae50b999c59d
Ramakrishna <ramakrishna.anguru@kony.com>
2015-12-30 19:26:23 +0530

    return statement added

--- samplecalci.c
+++ samplecalci.c
@@ -2,13 +2,13 @@ int sample_calci(int a,int b,int ch)
 {
 int res;
 if(ch==1)
-res=a+b;
+return a+b;
 else if(ch==2)
-res=a-b;
+return a-b;
 else if(ch==3)
-res=a*b;
+return a*b;
 else if(ch==4)
-res=a/b;
+return a/b;
 else 
 return 0;
 }
